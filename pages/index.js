ead7cfd0ebeb8c9f472a181282b788f2453ba88f
parzival-3x <bg193b@gmail.com>
2022-10-18 19:37:14 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Date from '../components/date'
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Welcome! My name is Benjamin Guzman, and I made this website using Nextjs. 
+          I am currently a freshman at the University of Houston, and did this project as an intern for Code[Coogs], a CS club on campus</p>
+        <p>
+          Visit my WIP Linkedln{' '}
+          <a href="https://www.linkedin.com/in/benjamin-guzman-4b6732235/">here</a>
+        </p>
+        <p>
+          The Github for this project avaliable <a href="https://github.com/parzival-3x/nextjs-blog">here</a>
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
---
+++ posts/ssg-ssr.md
@@ -0,0 +1,7 @@
+---
+title: 'Weekly Check-Up 2: 4/3-4/9'
+date: '2022-04-09'
+---
+
+This week, I focused on learning Next.JS. Fortunately, I found a tutorials for the framework on their website! I spent my week following the tutorial step by step to make the website you see now. It has gotten me really familiar
+with Next.js, along with some JS at the beginning. The following lesson is trying to convert this blog into typescript, so I will do that either this upcoming week or the following week. I'm not certain because I have a very busy week coming up, and I don't think I will be able to make meaningful progress next week.
